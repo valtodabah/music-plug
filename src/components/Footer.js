a320bb64351c802490286f230ff1561567bc8538
valtodabah <valentinodabah@gmail.com>
2024-11-26 16:16:07 -0300

    Fixed design detail

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,10 +2,8 @@ import Link from 'next/link';
 
 export default function Footer() {
     return (
-        <footer className="bg-gray-100 py-8">
-            <div className="pt-8 border-t border-gray-200 text-center">
+        <footer className="bg-gray-100 py-8 pt-8 border-t border-gray-200 text-center">
                 <p className="text-gray-600">&copy; 2024 Music Plug. All rights reserved.</p>
-            </div>
         </footer>
     );
 }
